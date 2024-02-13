fee71658e45c89b7c8f18c7fb3da8b772c868c0e
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-02-13 18:50:56 +0100

    Init lab 3 commit

---
+++ lab_3/src/main/java/com/przemyslawlewalski/app/GameStats.java
@@ -0,0 +1,35 @@
+package com.przemyslawlewalski.app;
+
+import java.io.Serializable;
+
+public class GameStats implements Serializable {
+    private int wins;
+    private int loses;
+    private String record;
+
+    public GameStats() {
+        this.setWins(0);
+        this.setLoses(0);
+        this.setRecord("");
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLoses() {
+        return loses;
+    }
+
+    public void setLoses(int loses) {
+        this.loses = loses;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+}
---
+++ lab_3/src/main/java/com/przemyslawlewalski/app/GameStatsMulti.java
@@ -0,0 +1,38 @@
+package com.przemyslawlewalski.app;
+
+public class GameStatsMulti extends GameStats {
+    private int seriesWins;
+    private int seriesLoses;
+    private String seriesRecord;
+
+    public GameStatsMulti() {
+        super();
+        this.setSeriesWins(0);
+        this.setSeriesLoses(0);
+        this.setSeriesRecord("");
+    }
+
+    public int getSeriesWins() {
+        return seriesWins;
+    }
+
+    public void setSeriesWins(int seriesWins) {
+        this.seriesWins = seriesWins;
+    }
+
+    public int getSeriesLoses() {
+        return seriesLoses;
+    }
+
+    public void setSeriesLoses(int seriesLoses) {
+        this.seriesLoses = seriesLoses;
+    }
+
+    public String getSeriesRecord() {
+        return seriesRecord;
+    }
+
+    public void setSeriesRecord(String seriesRecord) {
+        this.seriesRecord = seriesRecord;
+    }
+}
